cb991806f40dec64e1c4afa1ba1434e3bc06f0c0
gvardikos <george.vardikos@aisleahead.com>
2019-08-22 23:05:03 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by George Vardikos on 22/08/2019.
+//  Copyright © 2019 George Vardikos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
